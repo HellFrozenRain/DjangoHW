8848da94d45885a3cd3a5a0a54d1eaf7ec2b5e72
hellfrozenrain <cholinesterasa@gmail.com>
2022-05-27 13:06:19 +0300

    lesson_5

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('courses/', views.CoursesListView.as_view(), name='courses'),
     path('docsite/', views.DocSiteView.as_view(), name='docsite'),
     path('', views.IndexView.as_view(), name='index'),
-    path('login/', views.LoginView.as_view(), name='login'),
     path('news/', views.NewsView.as_view(), name='news'),
 ]
